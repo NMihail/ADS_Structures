e764577e3914f4c4e7d0178d0c98185a21403eaa
NMihail <nmihail535@gmail.com>
2023-12-24 20:30:00 +0300

    3/4

---
+++ DLS_Stack/DLS_Stack.c
@@ -0,0 +1,5 @@
+//
+// Created by Worker on 24.12.2023.
+//
+
+#include "DLS_Stack.h"
---
+++ DLS_Stack/DLS_Stack.h
@@ -0,0 +1,8 @@
+//
+// Created by Worker on 24.12.2023.
+//
+
+#ifndef ADS_STRUCTURES_DLS_STACK_H
+#define ADS_STRUCTURES_DLS_STACK_H
+
+#endif //ADS_STRUCTURES_DLS_STACK_H
